dce33a143d4f3fa535b73568c961ca91de13be61
JulianCedric <julian.pormentilla@gmail.com>
2020-04-22 14:43:47 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song 
+    attr_accessor :name, :artist, :genre 
+    @@count 
+    @@artist 
+    @@genres
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre 
+
+    end 
+
+    # def self.count 
+
+    # end
+
+
+end 
