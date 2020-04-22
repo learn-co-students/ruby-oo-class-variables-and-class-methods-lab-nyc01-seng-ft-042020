a22b0af4cf8c0dfa3ff7befdeac54a36cd74ce78
aromsa <annacbateman@gmail.com>
2020-04-22 14:58:50 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    attr_reader :name, :artist, :genre
+    def initialize (name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+    def self.count
+        @@count
+    end
+    def artists
+        @@artists
+    end
+    def genres
+        @@genres
+    end
+end
